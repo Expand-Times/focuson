98b5e97bb3438fbcd2d760cf59076dfa0bd1740f
zakirsujon <zakir021063008@gmail.com>
2026-02-06 15:16:41 +0600

    fix: home app option not working

---
+++ modules/launcher/android/src/main/java/com/expandtimes/modules/launcher/HomeLauncherActivity.kt
@@ -0,0 +1,10 @@
+package com.expandtimes.modules.launcher
+
+import android.app.Activity
+import android.os.Bundle
+
+class HomeLauncherActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
